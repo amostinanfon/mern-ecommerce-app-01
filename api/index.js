ec0425ab3f90d288209be924de0702675eaadc64
amostinanfon <amostinanfon17@gmail.com>
2022-04-09 08:57:03 +0100

    connection to DB ok

--- api/index.js
+++ api/index.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb+srv://admin:admi@cluster0.kk5cn.mongodb.net/ecommerce?retryWrites=true&w=majority')
+        .then(response =>console.log(response))
+        .catch(err => console.log(err))
+
+app.listen(5000, () => {
+    console.log("Backend server is running")
+})
