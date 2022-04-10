b6a2c53292641de82d9ef2e5eb38b8e5be3eeb3f
amostinanfon <amostinanfon17@gmail.com>
2022-04-10 15:03:35 +0100

    writing routes , testing post request

--- api/index.js
+++ api/index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+const userRoute = require('./routes/user')
+
+dotenv.config();
 
 
-mongoose.connect('mongodb+srv://admin:admi@cluster0.kk5cn.mongodb.net/ecommerce?retryWrites=true&w=majority')
-        .then(response =>console.log(response))
+mongoose.connect( process.env.MONGO_URL )
+        .then(() =>console.log('DB connected !!!'))
         .catch(err => console.log(err))
 
-app.listen(5000, () => {
+app.use(express.json())
+
+app.get("/api/users", () => {
+    console.log("Test réussi !!!")
+})
+
+app.use('/api/users', userRoute)
+
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running")
 })
