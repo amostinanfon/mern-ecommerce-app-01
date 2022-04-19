64bd66f21849aeed7a966aaf153b43b0bd47612e
amostinanfon <amostinanfon17@gmail.com>
2022-04-19 22:14:31 +0100

    creating and testing cart routes and middleware

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,7 +1,78 @@
 const router = require("express").Router();
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+const CryptoJS = require("crypto-js");
+const Product = require("../models/Product");
 
 
 
+// CREATE ..... METHODE DE CREATION DE Cartes
+
+router.post("/", verifyToken , async(req, res) => {
+    const newCart = new Cart(req.body)
+
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+        console.log(savedCart);
+    }catch(err){
+        res.status(500).json(err);
+        console.log(err);
+    }
+
+})
+
+// UPDATE  ::::::: mise à jour dess cartes
+router.put("/:id", verifyTokenAndAuthorization, async(req, res) =>{
+
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
+            $set : req.body
+        }, {new: true});
+
+        res.status(200).json(updatedCart)
+        console.log(updatedCart);
+    } catch (err) {
+        res.status(500).json(err)
+        console.log(err);
+    }
+})
+
+// // DELETE :::: SUPPRIMER UNE CARTE DANS L'API
+
+router.delete("/:id", verifyTokenAndAuthorization, async(req,res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("Carte supprimée . . .");
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+// // // GET USER CART  ... AVOIR UNE CARTE PRECISE
+
+router.get("/find/:userId", verifyTokenAndAuthorization , async(req,res) => {
+    try {
+        const cart =  await Cart.findOne({userId: req.params.userId});
+        res.status(200).json(cart)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+
+// // GET ALL USER CARTS ... AVOIR TOUTES LES CARTES UTILISATEURS
+
+router.get("/", verifyTokenAndAdmin ,async(req,res) => {
+
+    try {
+        const carts = await Cart.find();
+        return res.status(200).json(carts);
+    }catch(err){
+        res.status(500).json(err);
+        console.log(err);
+    }
+})
 
 
 
--- api/routes/order.js
+++ api/routes/order.js
@@ -1,8 +1,92 @@
 const router = require("express").Router();
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+const CryptoJS = require("crypto-js");
+const Order = require("../models/Order");
 
 
 
+// CREATE ..... METHODE DE CREATION DE Cartes
 
+router.post("/", verifyToken , async(req, res) => {
+    const newOrder = new Cart(req.body)
+
+    try {
+        const savedOrder= await newOrder.save();
+        res.status(200).json(newOrder);
+        console.log(newOrder);
+    }catch(err){
+        res.status(500).json(err);
+        console.log(err);
+    }
+
+})
+
+// UPDATE  ::::::: mise à jour dess cartes
+router.put("/:id", verifyTokenAndAuthorization, async(req, res) =>{
+
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(req.params.id, {
+            $set : req.body
+        }, {new: true});
+
+        res.status(200).json(updatedOrder)
+        console.log(updatedOrder);
+    } catch (err) {
+        res.status(500).json(err)
+        console.log(err);
+    }
+})
+
+// // DELETE :::: SUPPRIMER UNE CARTE DANS L'API
+
+router.delete("/:id", verifyTokenAndAdmin , async(req,res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.id);
+        res.status(200).json("Ordre supprimée . . .");
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+// // // GET USER Order  ... AVOIR UN ORDRE PRECIS
+
+router.get("/find/:userId", verifyTokenAndAuthorization , async(req,res) => {
+    try {
+        const cart =  await Cart.findOne({userId: req.params.userId});
+        res.status(200).json(cart)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+
+// GET ALL USER CARTS ... AVOIR TOUTES LES CARTES DES UTILISATEURS
+
+router.get("/", verifyTokenAndAdmin ,async(req,res) => {
+
+    try {
+        const oders = await Order.find();
+        return res.status(200).json(orders);
+    }catch(err){
+        res.status(500).json(err);
+        console.log(err);
+    }
+})
+
+
+// GET MONTHLY INCOME 
+router.get("/income", verifyTokenAndAdmin, async(req, res) =>{
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() -1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() -1))
+
+    try{
+
+    }catch(err){
+        res
+    }
+})
 
 
 
