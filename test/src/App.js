f96eeba7a6d8b2857ac1e8f5d8cedd6b26df5f96
amostinanfon <amostinanfon17@gmail.com>
2022-04-20 17:50:25 +0100

    going ahead with test component

--- test/src/App.js
+++ test/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
+import Pay  from './Pay.js'
+import Success from './Success.js'
+
+
+
 
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <h1>I love this game</h1>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path="/pay" element={<Pay/>} />
+        <Route path="/success" element={<Success/>} />
+      </Routes>
+    </Router>
   )
 }
