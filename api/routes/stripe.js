85951f4dc8bfd8c4c63ea19047054d33f2c1ef72
amostinanfon <amostinanfon17@gmail.com>
2022-04-21 06:15:01 +0100

    stripes tested and ready to be used

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,5 +1,6 @@
-const router = reauire("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const router = require("express").Router();
+//const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY_MY);
+const stripe = require("stripe")("sk_test_51KqbC1Iy4HUE4XIxv7bXpih22K38rePEfcrFM0fjm588yhqIKvsmbLMZfkkr71mlyWcl4qSB4FAtBfJHbLd708zW00Hqzu8Ell");
 
 
 router.post("/payment", (req,res) => {
@@ -12,9 +13,12 @@ router.post("/payment", (req,res) => {
         (stripeErr, stripeRes) => {
             if (stripeErr) {
                 res.status(500).json(stripeErr);
+                console.log(stripeErr)
             }else {
                 res.status(200).json(stripeRes);
             }
         }
     )
 })
+
+module.exports = router
