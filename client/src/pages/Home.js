5231cf846423cd196c24b56a60d32ee519fcde75
amostinanfon <amostinanfon17@gmail.com>
2022-04-24 19:30:23 +0100

    component products and product are ready

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
-// import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-// import Newsletter from "../components/Newsletter";
-// import Products from "../components/Products";
+import Products from "../components/Products";
 import Slider from "../components/Slider";
+// import Footer from "../components/Footer";
+// import Newsletter from "../components/Newsletter";
+
+
+
 
 const Home = () => {
   return (
@@ -14,12 +17,13 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
       {/*
-      <Products/>
       <Newsletter/>
       <Footer/> */}
     </div>
   );
 };
+
 
 export default Home;
