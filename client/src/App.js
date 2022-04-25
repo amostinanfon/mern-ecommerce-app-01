ffaf0dcaacd13df917fd9abeb96c79bdc05b9ae5
amostinanfon <amostinanfon17@gmail.com>
2022-04-26 00:18:44 +0100

    Product page is about to be finish : it's time to write some logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-//import Home from "./pages/Home";
-
-import ProductList from "./pages/ProductList";
-
+// import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 
 
 const App = () => {
-  return <ProductList/>;
+  return <Product/>;
 };
 
 
---
+++ client/src/pages/Product.js
@@ -0,0 +1,121 @@
+import styled from 'styled-components';
+import Announcement from '../components/Announcement';
+import Navbar from '../components/Navbar';
+
+
+
+const Container = styled.div`
+
+`
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`
+
+const ImgContainer = styled.div`
+        flex: 1;
+`
+const Image  = styled.img`
+    width: 100%;
+    height: 73vh;
+    object-fit: cover;
+    padding: 0 50px;
+
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+`
+const Title = styled.h2`
+    font-weight: 600;
+
+`
+const Desc = styled.p`
+margin: 20px 0px;
+
+`
+
+const Price = styled.span`
+    font-weight: 100%;
+    font-size: 40px;
+`
+
+
+const FilterContainer = styled.div`
+    width: 40%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content:space-between;
+`
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`
+const FilterTitle = styled.span`
+    font-style: 20px;
+    font-weight: 200px;
+`
+const FilterSizeOption = styled.option`
+`
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+
+`
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`
+
+const Product = () => {
+    return (<Container>
+                <Navbar />
+                <Announcement />
+                <Wrapper >
+                    <ImgContainer>
+                        <Image src="https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png"/>
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>Amos TINA NFON   </Title>
+                        <Desc>Proident officia consequat cillum qui eiusmod dolor in nostrud mollit culpa.
+                             Reprehenderit officia cupidatat laboris eu reprehenderit occaecat elit dolore 
+                             officia pariatur non ea. Ea minim nisi excepteur elit ad ipsum culpa dolore. 
+                             Excepteur labore veniam consequat commodo exercitation do consectetur duis. I
+                             ncididunt anim aliquip et sit dolore minim pariatur mollit eu reprehenderit magna.
+                              Duis enim irure dolore id aute culpa non.
+                            Ipsum consectetur ad enim anim in ut magna fugiat excepteur pariatur anim.
+
+                        </Desc>
+                        <Price>$ 20</Price>
+                        <FilterContainer>
+                            <Filter>
+                                <FilterTitle>Couleur</FilterTitle>
+                                <FilterColor color="black"/>
+                                <FilterColor color="darkblue"/>
+                                <FilterColor color="gray"/>
+                            </Filter>
+                            <Filter>
+                                <FilterTitle>Taille</FilterTitle>
+                              <FilterSize>
+                                    <FilterSizeOption>XS</FilterSizeOption>
+                                    <FilterSizeOption>S</FilterSizeOption>
+                                    <FilterSizeOption>XL</FilterSizeOption>
+                                    <FilterSizeOption>L</FilterSizeOption>
+                                    <FilterSizeOption>M</FilterSizeOption>
+                              </FilterSize>
+                            </Filter>
+                        </FilterContainer>
+                    </InfoContainer>
+                </Wrapper>
+    </Container>)
+}
+
+
+
+
+export default Product;
