a5f09824b0d73ef4933a13f3c2c5aaf4ebd14d7d
amostinanfon <amostinanfon17@gmail.com>
2022-05-20 11:38:14 +0100

    connect admin with redux , login , localstorage

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -7,6 +7,7 @@ export const login = async (dispatch, user) =>{
     dispatch(loginStart());
     try {
         const res = await publicRequest.post("/auth/login", user)
+        console.log(res.data);
         dispatch(loginSuccess(res.data))
     } catch (error) {
         dispatch(loginFailure())
