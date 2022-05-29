ed7f63fd9d762d3489e4a5949ffe2d0fcc4e946a
amostinanfon <amostinanfon17@gmail.com>
2022-05-29 18:15:16 +0100

    connecting app with firebase to upload file

--- test/admin/src/pages/newProduct/NewProduct.jsx
+++ test/admin/src/pages/newProduct/NewProduct.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import "./newProduct.css";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from "../../firebase";
+
 
 export default function NewProduct() {
 
@@ -21,8 +24,49 @@ export default function NewProduct() {
 
   const handleClick = (e) => {
     e.preventDefault();
-    
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+
+   
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    console.log(uploadTask);
+// Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+      default:
+    }
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+    });
   }
+);
+  }
+
+  console.log(file);
 
   return (
     <div className="newProduct">
@@ -30,8 +74,10 @@ export default function NewProduct() {
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" 
-          onChange={e => setFile(e.target.files)[0]}
+          <input 
+            type="file" 
+            id="file" 
+            onChange={e => setFile(e.target.files[0])}
         />
         </div>
         <div className="addProductItem">
